a3a6d21cbac94ae43bec4f571bc9131329e2f2fd
masondo <plantsocks@gmail.com>
2017-02-23 13:44:00 -0600

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,4 @@
+<script type= "text/javascript>
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,3 +25,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += "active";
 }
+</script>
